fa70e98f9fe38a00775834388ec6e5005d85c295
xudan <xudan@apowo.com>
2022-02-17 15:14:26 +0800

    fix

--- src/components/LandsTileMap/landsTileMap.vue
+++ src/components/LandsTileMap/landsTileMap.vue
@@ -127,36 +127,4 @@ export default {
 </script>
 
 <style lang="scss" rel="stylesheet/scss"  scoped>
-.hg-loading-wrapper {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 99;
-  background-color: #fff;
-}
-
-.hg-cssload-container {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  margin: auto;
-  text-align: center;
-}
-
-.hg-cssload-speeding-wheel {
-  margin: 0 auto;
-  border-radius: 50%;
-  animation: cssload-spin 575ms infinite linear;
-}
-
-@keyframes cssload-spin {
-  100% {
-    transform: rotate(360deg);
-  }
-}
 </style>
