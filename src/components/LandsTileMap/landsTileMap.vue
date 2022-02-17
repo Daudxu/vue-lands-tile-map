511c55b494e521d5fd2e7f07e491957954dd591b
xudan <xudan@apowo.com>
2022-02-17 15:06:44 +0800

    lands tile init map

--- src/components/LandsTileMap/landsTileMap.vue
+++ src/components/LandsTileMap/landsTileMap.vue
@@ -16,47 +16,74 @@ export default {
   name: 'landsTileMap',
   data () {
     return {
-      initMapmatrix:
-        [
-          [37, 37, 37, 37, 37, 37, 37, 37],
-          [37, 37, 37, 37, 37, 37, 37, 37],
-          [37, 37, 37, 37, 37, 37, 37, 37],
-          [37, 37, 37, 37, 37, 37, 37, 37],
-          [37, 37, 37, 37, 37, 37, 37, 37],
-          [37, 37, 37, 37, 37, 37, 37, 37],
-          [37, 37, 37, 37, 37, 37, 37, 37],
-        ],
       myCanvas: null,
       ctx: null,
-      canvasWidth: window.innerWidth,
-      canvasHeight: window.innerHeight,
-      tileSize: 16,
-      imgScale: 1,
-      imgX: window.innerWidth / 12,
-      imgY: 0,
     };
   },
   props: {
-    color: {
+    initMapmatrix: {
+      type: Array,
+      default: () => [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ]
+    },
+    tiledDigitalColormap: {
+      type: Array,
+      default: () => [
+        { 0: '#138535' },
+        { 1: '#101566' },
+        { 2: '#0070c0' }
+      ]
+    },
+    canvasWidth: {
+      type: Number,
+      default: window.innerWidth,
+    },
+    canvasHeight: {
+      type: Number,
+      default: window.innerHeight,
+    },
+    tileSize: {
+      type: Number,
+      default: 32,
+    },
+    tileScale: {
+      type: Number,
+      default: 1,
+    },
+    startPaintingX: {
+      type: Number,
+      default: window.innerWidth / 12,
+    },
+    startPaintingY: {
+      type: Number,
+      default: 0,
+    },
+    gridLineColor: {
       type: String,
-      default: '#2a8ee3'
+      default: '#c8efd4'
     },
-    diam: {
-      type: Number,
-      default: 50
+    gridTileColor: {
+      type: String,
+      default: '#ffffff'
     },
-    size: {
-      type: Number,
-      default: 3
-    }
   },
   mounted () {
     this.myCanvas = this.$refs.titleMapCanvas;
     this.ctx = this.myCanvas.getContext('2d');
     var _this = this;
-    setTimeout(() => {
-      _this.drawImage(this.initMapmatrix);
-    }, 1000);
+    _this.drawImage(this.initMapmatrix);
+    // window.onresize = function () {
+    //   _this.resizeCanvas();
+    // };
   },
   methods: {
     resizeCanvas () {
@@ -70,20 +97,30 @@ export default {
       var _this = this;
       _this.ctx.clearRect(0, 0, _this.myCanvas.width, _this.myCanvas.height);
       this.ctx.lineWidth = 1;
-      this.ctx.strokeStyle = '#a0a0a0';
+      this.ctx.strokeStyle = this.gridLineColor;
       this.ctx.beginPath();
       for (let row = 0; row < map.length; row++) {
         for (let column = 0; column < map[row].length; column++) {
-          // this.ctx.fillText(column, this.imgX + (column * (this.tileSize * this.imgScale)), (this.tileSize * this.imgScale));
-          // this.ctx.fillText(column, this.imgX + (this.tileSize * this.imgScale), (row * (this.tileSize * this.imgScale)));
-          var SizeimgX = this.imgX + (column * this.tileSize * this.imgScale);
-          var SizeimgY = this.imgY + (row * this.tileSize * this.imgScale);
-          this.ctx.fillStyle = '#5e389a66';
-          this.ctx.fillRect(SizeimgX, SizeimgY, (1 * this.tileSize * this.imgScale), (1 * this.tileSize * this.imgScale));
-          this.ctx.rect(SizeimgX, SizeimgY, (1 * this.tileSize * this.imgScale), (1 * this.tileSize * this.imgScale));
+          // this.ctx.fillText(column, this.startPaintingX + (column * (this.tileSize * this.imgScale)), (this.tileSize * this.imgScale));
+          // this.ctx.fillText(column, this.startPaintingY + (this.tileSize * this.imgScale), (row * (this.tileSize * this.imgScale)));
+          const tileMapVal = map[row][column];
+          this.ctx.fillStyle = this.getGridTileColor(tileMapVal);
+          var SizeimgX = this.startPaintingX + (column * this.tileSize * this.tileScale);
+          var SizeimgY = this.startPaintingY + (row * this.tileSize * this.tileScale);
+          this.ctx.fillRect(SizeimgX, SizeimgY, (1 * this.tileSize * this.tileScale), (1 * this.tileSize * this.tileScale));
+          this.ctx.rect(SizeimgX, SizeimgY, (1 * this.tileSize * this.tileScale), (1 * this.tileSize * this.tileScale));
           this.ctx.stroke();
         }
       }
+    },
+    getGridTileColor (tileMapVal) {
+      var gridTileColor = this.gridTileColor;
+      this.tiledDigitalColormap.forEach((item) => {
+        if (typeof (item[tileMapVal]) !== 'undefined' && item[tileMapVal]) {
+          gridTileColor = item[tileMapVal];
+        }
+      });
+      return gridTileColor;
     }
   }
 };
