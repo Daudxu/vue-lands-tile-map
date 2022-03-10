39d3f1588c6f1dc931bd2a8f801ea6375f08419a
xudan <xudan@apowo.com>
2022-03-10 15:42:21 +0800

    test plugins

--- examples/main.js
+++ examples/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueLandsTileMap from '../packages/index'
+// import VueLandsTileMap from '../packages/index'
 // import VueModelViewer from '../index.js'
 // import VueModelViewer from '../lib/vue-lands-tile-map.umd.js'
-// import VueLandsTileMap from 'vue-lands-tile-map'
+import VueLandsTileMap from 'vue-lands-tile-map'
 Vue.config.productionTip = false
 Vue.use(VueLandsTileMap);
 new Vue({
