7ba3c2b8217f7dbf03332e5dd11fe2a0cf6bd520
Daudxu <875126243@qq.com>
2022-03-10 06:02:05 +0000

    Deploying to gh-pages from  @ 847d693ae6f38a02b299a11ed1df3b610cd1d8fc 🚀

--- index.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./lib/vue-lands-tile-map.umd.min.js')
-} else {
-  module.exports = require('./lib/vue-lands-tile-map.umd.js')
-}
--- packages/vueLandsTileMap/index.js
+++
@@ -1,9 +0,0 @@
-// 导入组件，组件必须声明 name
-import VueLandsTileMap from './src/main.vue'
-
-// 为组件添加 install 方法，用于按需引入
-VueLandsTileMap.install = function (Vue) {
-  Vue.component(VueLandsTileMap.name, VueLandsTileMap)
-}
-
-export default VueLandsTileMap
