8fe1cfb1943e5a2fa21157e1d9675e9e0ac110dd
xudan <xudan@apowo.com>
2022-03-10 14:32:27 +0800

    update version

--- examples/main.js
+++ examples/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueLandsTileMap from '../packages/index'
 // import VueModelViewer from '../index.js'
-// import VueModelViewer from '../lib/vue-model-viewer.umd.js'
-// import VueLandsTileMap from 'vue-model-viewer'
+// import VueModelViewer from '../lib/vue-lands-tile-map.umd.js'
+// import VueLandsTileMap from 'vue-lands-tile-map'
 Vue.config.productionTip = false
 Vue.use(VueLandsTileMap);
 new Vue({
